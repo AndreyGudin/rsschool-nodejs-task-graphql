1682f6fdab480d04454d392030907534dfa9e6ea
Andrey Gudin <aeon150791@gmail.com>
2023-01-26 09:06:40 +0400

    feat: add logic to memberstype route

--- src/routes/member-types/index.ts
+++ src/routes/member-types/index.ts
@@ -8,7 +8,9 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
 ): Promise<void> => {
   fastify.get('/', async function (request, reply): Promise<
     MemberTypeEntity[]
-  > {});
+  > {
+    return await fastify.db.memberTypes.findMany();
+  });
 
   fastify.get(
     '/:id',
@@ -17,7 +19,14 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<MemberTypeEntity> {}
+    async function (request, reply): Promise<MemberTypeEntity> {
+      const result = (await fastify.db.memberTypes.findOne({
+        key: "id",
+        equals: request.params.id,
+      })) as MemberTypeEntity;
+      if (!result) reply.notFound();
+      return result;
+    }
   );
 
   fastify.patch(
@@ -28,7 +37,14 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<MemberTypeEntity> {}
+    async function (request, reply): Promise<MemberTypeEntity> {
+      const result = await fastify.db.memberTypes.change(
+        request.params.id,
+        request.body
+      );
+      if (!result) reply.badRequest();
+      return result;
+    }
   );
 };
 
