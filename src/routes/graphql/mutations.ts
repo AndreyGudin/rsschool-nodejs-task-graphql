86f6d528241a70328c88ec640136a88a75024a49
Andrey Gudin <aeon150791@gmail.com>
2023-01-29 17:39:28 +0400

    feat: add mutation create profile

--- src/routes/graphql/mutations.ts
+++ src/routes/graphql/mutations.ts
@@ -1,25 +1,51 @@
-import {
-  GraphQLObjectType,
-  GraphQLString,
-} from "graphql";
+import { GraphQLID, GraphQLObjectType, GraphQLString } from "graphql";
+import { ProfileEntity } from "../../utils/DB/entities/DBProfiles";
 
-import { User } from './graphql-types';
+import { Profile, User } from "./graphql-types";
 
 export default new GraphQLObjectType({
-  name: 'Mutations',
+  name: "Mutations",
   fields: {
     createUser: {
       type: User,
       args: {
-        firstName: {type: GraphQLString},
-        lastName: { type: GraphQLString},
-        email: { type: GraphQLString},
+        firstName: { type: GraphQLString },
+        lastName: { type: GraphQLString },
+        email: { type: GraphQLString },
       },
-      resolve: function(parent, {firstName, lastName, email}, contextValue) {
-        console.log("contextValue",contextValue);
-        return contextValue.db.users.create({firstName, lastName, email});
+      resolve: function (parent, { firstName, lastName, email }, contextValue) {
+        return contextValue.db.users.create({ firstName, lastName, email });
+      },
+    },
+    createProfile: {
+      type: Profile,
+      args: {
+        avatar: { type: GraphQLString },
+        sex: { type: GraphQLString },
+        birthday: { type: GraphQLString },
+        country: { type: GraphQLString },
+        street: { type: GraphQLString },
+        city: { type: GraphQLString },
+        memberTypeId: { type: GraphQLString },
+        userId: { type: GraphQLID },
+      },
+      resolve:async function (
+        parent,
+        { avatar, sex, birthday, country, street, city, memberTypeId, userId },
+        contextValue
+      ) {
+        const body = { avatar, sex, birthday, country, street, city, memberTypeId, userId };
+        const memberType = await contextValue.db.memberTypes.findOne({key:'id', equals: memberTypeId});
+        const profile = await contextValue.db.profiles.findOne({key:'userId', equals:userId});
+        let result:ProfileEntity={} as ProfileEntity;
+        if (memberType && !profile) {
+          result = await contextValue.db.profiles.create(body);
+          if (!result) return ;
+      } else {
+        return;
       }
-
-    }
-  }
-})
+        return result;
+      },
+    },
+  },
+});
